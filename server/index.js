17b50efff1284a91ab2003ce70cd13a7e89f875b
Markus Weberndorfer <markus@weberndorfer.eu>
2024-02-21 23:09:12 +0100

    feat: added ssh func

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
+// app
 const express = require('express');
-require('dotenv').config()
 const app = express();
 const port = 3000;
+
+// node-ssh
+const { NodeSSH } = require('node-ssh');
+const ssh = new NodeSSH();
 
 app.get('/', (req, res) => {
     access(req.hostname);
     res.send("API");
 })
 
-app.get('//test/[id]', (req, res) => {
-    access(req.hostname + req.id);
-    res.send("schlecht");
+app.get('//containers', async (req, res) => {
+    await ssh
+        .connect({
+            host: '10.0.0.71',
+            username: 'markus',
+            privateKeyPath: '/home/markus/.ssh/id_rsa'
+        })
+        .then(function() {
+            ssh.execCommand('docker container ls -a --format  \'{{ json . }}\'', { cwd: '.'
+            }).then((result) => {
+                console.log("Result available at " + new Date())
+                res.json(result.stdout);
+            });
+        });
 })
 
 app.listen(port, () => {
